659f1ce7c0eb44dd4637a442967b152e216308ef
Seth Mulia <smulia@r2hstudent.com>
2022-01-24 21:28:03 -0500

    java challenge

---
+++ script.js
@@ -0,0 +1,11 @@
+/*EASY: Write a function that would allow you to do this:
+
+var run = exercise('running');
+
+console.log(run()); // prints "Today's exercise: running"
+
+var swim = exercise('swimming');
+
+console.log(swim()); // prints "Today's exercise: swimming"
+*/
+
